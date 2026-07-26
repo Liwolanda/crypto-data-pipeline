57fa9494735bb9efa83b0ebd704c38fbf790f1bb
Leonardo <rabbitsb98@gmail.com>
2026-07-25 23:08:56 -0300

    Adiciona ajustes de teste

--- Projeto_05/src/silver.py
+++ Projeto_05/src/silver.py
@@ -1,6 +1,7 @@
 import logging
 
 from pyspark.sql import DataFrame, SparkSession
+import pyspark.sql.functions as F
 
 
 PRIMARY_KEYS = {
@@ -65,6 +66,115 @@ def remove_null_primary_keys(
     return valid_df
 
 
+def transform_order_details(
+    df: DataFrame
+) -> DataFrame:
+    """Converte as colunas monetárias de order_details para double."""
+
+    monetary_columns = {
+        "unitprice",
+        "totalprice",
+    }
+
+    transformed_df = df.select(
+        *[
+            (
+                F.regexp_replace(
+                    F.col(column_name),
+                    ",",
+                    "."
+                )
+                .cast("double")
+                .alias(column_name)
+            )
+            if column_name in monetary_columns
+            else F.col(column_name)
+            for column_name in df.columns
+        ]
+    )
+
+    return transformed_df
+
+
+
+def transform_orders(
+    df: DataFrame
+) -> DataFrame:
+    """Trata as colunas numéricas e temporais da tabela orders."""
+
+    transformed_df = df.withColumn(
+        "totalbasket",
+        F.regexp_replace(
+            F.col("totalbasket"),
+            ",",
+            "."
+        ).cast("double")
+    )
+
+    transformed_df = transformed_df.withColumn(
+        "date_",
+        F.to_date(
+            F.col("date_"),
+            "dd/MM/yyyy"
+        )
+    )
+
+    return transformed_df
+
+def transform_customers(
+    df:DataFrame
+) -> DataFrame:
+
+    ## transformacao birthdate = date
+
+    transformed_df = df.withColumn(
+        "birthdate",
+        F.to_date(
+            F.col("birthdate"),
+            "dd/MM/yyyy"
+        )
+    )
+
+    return transformed_df
+
+def transform_branches(
+        df:DataFrame
+) -> DataFrame:
+
+    ## Transformacao do lat e log = colocar ponto
+
+    transformed_df = df.withColumn(
+        "lat",
+        F.col("lat").cast("double") / 100000000
+    ).withColum(
+        F.col("lon").cast("double") / 100000000
+    )
+    return transformed_df
+    
+
+
+def apply_table_rules(
+    df: DataFrame,
+    table_name: str
+) -> DataFrame:
+
+    if table_name == "orders":
+        return transform_orders(df)
+
+    if table_name == "order_details":
+        return transform_order_details(df)
+
+    if table_name == "birthdate":
+        return transform_customers(df)
+
+    if table_name == "branch_id"
+        return transform_branches(df) 
+
+    return df
+
+
+
+
 def save_silver(
     df: DataFrame,
     silver_path: str
@@ -110,6 +220,11 @@ def process_silver(
         silver_df = remove_null_primary_keys(
             silver_df,
             primary_keys
+        )
+
+        silver_df = apply_table_rules(
+            silver_df,
+            table_name
         )
 
         save_silver(
---
+++ Projeto_05/src/test_orders.py
@@ -0,0 +1,15 @@
+#%%
+
+import pandas as pd
+from pathlib import Path
+
+df = pd.read_csv("../data/raw/Branches.csv")
+df.dtypes
+
+
+print("=" * 80)
+
+
+
+
+# %%
